13275d0f6ca8e6bbe83d6ef1638a76d1985219bc
Eduardo Ahumada <pulibass@users.noreply.github.com>
2022-06-03 00:29:26 -0300

    agregando itemCount

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import Header from './Components/Header/Header';
+import ItemCount from './Components/ItemCount/ItemCount';
 import "./css/App.css";
 function App() {
   return (
     <div className="App">
       <Header />
+
+      <ItemCount />
     </div>
 
   );
--- src/Components/ItemCount/ItemCount.js
+++ src/Components/ItemCount/ItemCount.js
@@ -1,1 +1,60 @@
 import React from 'react';
+import "../../css/App.css";
+
+
+export default class ItemCount extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            count: 0
+        };
+    }
+
+    incrementCount() {
+        this.setState({
+            count: this.state.count + 1
+        });
+    }
+    decrementCount() {
+
+        if (this.state.count <= 0) return;
+        this.setState({
+            count: this.state.count - 1
+
+
+        });
+    }
+
+    addToCart() {
+        alert("agregado al carro")
+    }
+
+    render() {
+        return (
+            <div className='containerItemCount'>
+                <h1>Item Count</h1>
+
+                <div className='countItem'>
+
+                    <button onClick={() => this.decrementCount()}>
+                        -
+                    </button>
+                    <p className='count'>
+                        {this.state.count}
+                    </p>
+                    <button onClick={() => this.incrementCount()}>
+                        +
+                    </button>
+
+                </div>
+                <div className='addToCart'>
+                    <button onClick={() => this.addToCart()}>
+                        Agregar al carro
+                    </button>
+                </div>
+
+            </div>
+
+        );
+    }
+}
